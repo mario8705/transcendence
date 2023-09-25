a7412531ac6b7ff9c679825b543b09b83680c085
ycurbill <108531077+ycurbill@users.noreply.github.com>
2023-09-25 11:08:46 +0200

    Doing profile page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,61 @@
 //import { useState } from 'react'
-import './App.css'
+import { Component } from "react";
+import './App.css';
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+import Profile from "./components/Profile/Profile";
 
-function App() {
-  //const [count, setCount] = useState(0)
-
-  return (
-    <>
-      
-    </>
-  )
+interface InitialState {
+  route: string;
+  isSignedIn: boolean;
 }
 
-export default App
+interface Props {}
+
+const initialState: InitialState = {
+  route: 'profile', // Change when signin is done
+  isSignedIn: true,
+};
+
+class App extends Component<Props, InitialState> {
+  constructor(props: Props) {
+    super(props);
+    this.state = initialState;
+  }
+
+  onRouteChange = (route: string): void => {
+    if (route === 'signout') {
+      this.setState(initialState);
+    } else if (route === 'game' || route === 'profile' || route === 'chat' || route === 'pong') {
+      this.setState({isSignedIn: true});
+    }
+    this.setState({route: route});
+  }
+
+  render() {
+    const { isSignedIn, route }: {isSignedIn: boolean; route: string } = this.state;
+    
+    return (
+      <>
+        {
+          (route === 'game' || route === 'chat' || route === 'profile' || route === 'pong') 
+          ? ( 
+            <div className="App">
+              <Router>
+                <Routes>
+                  <Route path='profile' element={<Profile onRouteChange={this.onRouteChange} />} />
+                </Routes>
+              </Router>
+            </div>
+          )
+          : (
+            route === 'signin' 
+            ? '' /* <Signin onRouteChange={this.onRouteChange}/> */
+            : '' /* <Register={this.onRouteChange)/> */
+          )
+        }
+      </>
+    );
+  }
+}
+
+export default App;
