5bc8932a6f81e1c07374ea62921963c46fbe6e76
Charline <Cha333macgmail.com>
2023-09-27 20:27:21 +0200

    add stats

---
+++ frontend/src/components/Stats/Stats.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import './Stats.css';
+import { IoAnalyticsOutline } from "react-icons/io5";
+import { GiWingedSword } from "react-icons/gi";
+import { BsStars } from "react-icons/bs";
+
+const Stats: React.FC = () => {
+    return  (
+
+        <div className='parent'>
+            <div className='main'>
+                <div className='statt'>
+                    <p className='title'>Level</p>
+                    <div className='middle'>1</div>
+                    <div className='rank'>Rank: <span>gold</span></div>
+                   <GiWingedSword className="icon"/>
+                </div>
+                <div className='statt'>
+                    <p className='title'>Win streak</p>
+                    <div className='middle'>16</div>
+                   <BsStars className="icon"/>
+                </div>
+                <div className='statt'>
+                    <p className='title'>Ratio</p>
+                    <div className='middle'>8.0</div>
+                    <div className='score'>
+                        <p className='win'>Win: <span>32</span></p>
+                        <p className='lose'>Lose: <span>4</span></p>
+                    </div>
+                   <IoAnalyticsOutline className="icon"/>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Stats;
