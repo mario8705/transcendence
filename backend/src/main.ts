09c7e6dbedb76ebcd4035e4563b8f113e6b4bf80
Alexis Lavaud <alexislavaud191@gmail.com>
2023-10-10 04:07:38 +0200

    Backend authentication working with 42 OAuth

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  	const app = await NestFactory.create(AppModule);
+	app.setGlobalPrefix('api'); /* Starts every route with /api and then api version (eg. /api/v1/users/@me) */
+	app.enableVersioning({
+		type: VersioningType.URI,
+	});
+
+	await app.listen(3000);
 }
 bootstrap();
