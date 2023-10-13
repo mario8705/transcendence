54e89ed13177d07aa33a25ff0d94a87028cc96b7
ChaDutel <cha333mac@gmail.com>
2023-10-13 14:13:26 +0200

    add DoubleAuth and fix MainButton with adding icon and changing mode

--- frontend/src/components/MainButton/MainButton.tsx
+++ frontend/src/components/MainButton/MainButton.tsx
@@ -3,12 +3,15 @@ import './MainButton.css';
 
 interface Props {
     buttonName: string;
+    mode?: number;
+    onClick?: (newMode: number) => void;
+    icon?: React.ReactNode;
 }
 
-const MainButton: React.FC<Props> = ({buttonName}) => {
+const MainButton: React.FC<Props> = ({buttonName, mode, onClick, icon}) => {
     return (
         <div className=''>
-            <button className='mainBtn' >{buttonName}</button>
+            <button className='mainBtn' onClick={() => onClick && onClick(mode || 0)} > {icon} {buttonName} {icon}</button>
         </div>
     );
 };
