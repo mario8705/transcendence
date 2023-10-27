95c917f4543669748030602d7cc88151bcfbfc73
tulipe <tulipe@fulls-MacBook-Air.local>
2023-10-27 15:41:14 +0200

    Add basic wrapper

---
+++ frontend/src/components/Chat/Chat.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Chat.css';
+
+const Chat: React.FC = () => {
+	return (
+		<div className='chat-wrapper' >
+		</div>
+	)
+}
+
+export default Chat;
--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -5,6 +5,7 @@ import { LoginPage } from './pages/LoginPage';
 import Profile from './components/Profile/Profile';
 import Game from './components/Game/Game';
 import FriendList from './components/FriendList/FriendList';
+import Chat from './components/Chat/Chat';
 
 export const router = createBrowserRouter([
     {
@@ -30,6 +31,10 @@ export const router = createBrowserRouter([
             {
                 path: '/game',
                 element: <Game className="gameCanvas" width={800} height={600} />,
+            },
+            {
+                path: '/chat',
+                element: <Chat />
             },
         ],
     },
