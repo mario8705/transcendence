d5302804a602138a531a99ea0f5331e2eb813be0
tulipe <tulipe@fulls-MacBook-Air.local>
2023-11-05 17:07:45 +0100

    Add channels component only static

--- frontend/src/components/Chat/Chat.tsx
+++ frontend/src/components/Chat/Chat.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './Chat.css';
+import ChatChannels from './Channels/ChatChannels';
 
 const Chat: React.FC = () => {
 	return (
 		<div className='chat-wrapper' >
+			<ChatChannels />
 		</div>
 	)
 }
