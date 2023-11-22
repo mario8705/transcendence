97e39e4b3aea5b734fdc3c12f2477ff726efc050
tulipe <tulipe@fulls-MacBook-Air.local>
2023-11-22 16:41:14 +0100

    Delete and comment profile temporary

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,11 +5,10 @@ import { SocketModule } from './socket/socket.module';
 import { UsersModule } from './chat/users/users.module';
 import { RoomsModule } from './chat/rooms/rooms.module';
 import { TestModule } from './users/users.module';
-import { ProfileModule } from './profile/profile.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ AuthModule, GameModule, SocketModule, UsersModule, RoomsModule, ProfileModule, TestModule, PrismaModule ],
+  imports: [ AuthModule, GameModule, SocketModule, UsersModule, RoomsModule, TestModule, PrismaModule ],
   controllers: [],
   providers: [],
 })
