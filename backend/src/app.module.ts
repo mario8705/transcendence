6e69eabdecb444f786117dd7aa2f3e57fc45e794
Hloise <heloisemarco@gmail.com>
2023-11-29 15:28:49 +0100

    je suis bete

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,15 +1,31 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
+// import { AuthModule } from './auth/auth.module';
 import { GameModule } from './game/game.module';
 import { SocketModule } from './socket/socket.module';
-import { UsersModule } from './chat/users/users.module';
-import { RoomsModule } from './chat/rooms/rooms.module';
-import { TestModule } from './users/users.module';
+// import { TestModule } from './users/users.module';
 
 @Module({
-  imports: [ AuthModule, GameModule, SocketModule, UsersModule, RoomsModule, TestModule ],
+  imports: [
+	GameModule, 
+	SocketModule,
+], //AuthModule, TestModule
   controllers: [],
   providers: [],
 })
 
 export class AppModule {}
+
+// imports: [
+//     AuthModule,
+//     GameModule,
+//     SocketModule,
+//     UsersModule,
+//     RoomsModule,
+//     ProfileModule,
+//     FriendsModule,
+//     TestModule,
+//     PrismaModule,
+//   ],
+//   controllers: [],
+//   providers: [],
+// })
